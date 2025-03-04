d8ebf1457e20c8ac1d6fc5c0977f060379cabc48
Davi <herculanodavi@gmail.com>
2025-03-04 16:01:00 +0100

    feat: add sync read

--- lib/dynamixel/instruction_packet.c
+++ lib/dynamixel/instruction_packet.c
@@ -4,13 +4,14 @@
 
 #include "dynamixel/protocol.h"
 
-dxl_err_t dxl_make_sync_write_i16(dxl_inst_packet_t *packet, uint16_t address, uint8_t *ids, uint16_t num_ids, int16_t *values)
+dxl_err_t dxl_make_sync_write_i16(dxl_inst_packet_t *packet, uint16_t address, const dxl_id_t *ids,
+				  uint16_t num_ids, const int16_t *values)
 {
 	if (ids == NULL || values == NULL) {
 		return DXL_PTR_ERROR;
 	}
 	// Fill parameters with data
-	uint16_t needed_params_len = num_ids * (sizeof(uint8_t)     // id
+	uint16_t needed_params_len = num_ids * (sizeof(dxl_id_t)    // id
 						+ sizeof(int16_t)); // value
 	if (DXL_MAX_NUM_PARAMS < needed_params_len) {
 		// Provided buffer is too small
@@ -24,7 +25,29 @@ dxl_err_t dxl_make_sync_write_i16(dxl_inst_packet_t *packet, uint16_t address, u
 	packet->params_length = 3 * num_ids;
 	packet->id = DXL_ID_BROADCAST;
 	packet->instruction = DXL_INST_SYNC_WRITE;
-    packet->protocol_version = DYNAMIXEL_PROTOCOL_VERSION;
+	packet->protocol_version = DYNAMIXEL_PROTOCOL_VERSION;
+
+	return DXL_OK;
+}
+
+dxl_err_t dxl_make_sync_read_i16(dxl_inst_packet_t *packet, uint16_t address, const dxl_id_t *ids,
+				 uint16_t num_ids)
+{
+	if (ids == NULL) {
+		return DXL_PTR_ERROR;
+	}
+	// Fill parameters with data
+	uint16_t needed_params_len = num_ids * sizeof(dxl_id_t);
+	if (DXL_MAX_NUM_PARAMS < needed_params_len) {
+		// Provided buffer is too small
+		return DXL_ERROR;
+	}
+
+	memcpy(packet->params, ids, num_ids);
+	packet->params_length = num_ids;
+	packet->id = DXL_ID_BROADCAST;
+	packet->instruction = DXL_INST_SYNC_READ;
+	packet->protocol_version = DYNAMIXEL_PROTOCOL_VERSION;
 
 	return DXL_OK;
 }
